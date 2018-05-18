eb67a535c55ff280a849fa77eedcfd37f1c2a0d1
RitaSQTan <35492829+RitaHT@users.noreply.github.com>
2018-05-19 00:14:15 +0800

    Update and rename ACSL 2017 Practice to ACSL Practice/2017/#9_Capsule.java

---
+++ ACSL Practice/2017/#9_Capsule.java
@@ -0,0 +1,45 @@
+//check it tomorrow
+
+import java.io.*;
+import java.util.*;
+
+public class Capsule{
+
+  public static void main(String[]args) throws IOException{
+    BufferedReader f = new BufferedReader(new FileReader("input.txt"));
+    //do we have a .txt in the actual contest? or just a string line that can copy?
+    
+    String line = f.readLine();
+    
+    while(line != null){
+      String[] vars = line.split(",");
+      int row = Integer.parseInt(vars[0]);
+      int col = Integer.parseInt(vars[1]);
+      
+      /*1. create the original capsule
+       *by cutting the vars[3] into (row) pieces
+       * converting each substring into dec then into binary
+       * marking each cell's state(really??)
+       */
+      
+      /*2. 
+       * */   
+       
+    }
+    
+  }
+}
+
+class Cell{
+  //attributes
+  int row;
+  int col;
+  
+  boolean top;
+  boolean right;
+  boolean bot;
+  boolean left;
+  
+}
+
+
